f079a0557b1a196ef790ffdaf8463df9d4123f3f
gauravjha0711 <gauravjha0711@gmail.com>
2026-02-24 10:18:29 +0530

    First file of javascript

---
+++ day2_24/first.js
@@ -0,0 +1,15 @@
+var name = "Gaurav Jha";
+var age = 22;
+var isStudent = true;
+var hobbies = ["coding", "gaming", "traveling"];
+var address = {
+    city: "Patna",
+    state: "Bihar",
+    country: "India"
+};
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+console.log("Hobbies:"+ hobbies.join(", "));
+console.log("Address:", address);
